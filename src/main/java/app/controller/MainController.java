04b59d1006ed3c0d45e1f6159a636b047fa31c69
jang <jang_inhwa@microad.co.jp>
2019-01-23 12:30:39 +0900

    Wip on controller, application context

---
+++ src/main/java/app/controller/MainController.java
@@ -0,0 +1,14 @@
+package app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+
+  @RequestMapping(value = {"", "/everydays"}, method = RequestMethod.GET)
+  public String showList() {
+    return "everydays";
+  }
+}
